45cd7620c4dbec98d3c918f0ee8b142d2e3e2457
loyd0 <samloyd@hotmail.co.uk>
2023-09-05 14:09:08 +0100

    completed bare template

---
+++ README.md
@@ -0,0 +1,9 @@
+# OFI Templates
+These are html + tailwind ui templates for pardot landing pages. 
+
+## Run Script
+Right click the file and "run with live server"
+
+
+## Boot Up Tailwind 
+`npx tailwindcss -i ./input.css -o ./dist/output.css --watch` or `npm run build`
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "primary": "#F78C2A", // orange
+        "secondary": "#940065", // purple
+      }
+    },
+  },
+  plugins: [],
+}
+
